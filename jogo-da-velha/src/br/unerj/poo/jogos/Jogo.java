766e8ab6ec0f85f220c429b2e8558e951cdbfba0
Andre Vinicius Castoldi <avcastoldi@gmail.com>
2010-03-29 21:16:15 -0300

    Inclusao de testes automatizados para a classe Jogo

--- jogo-da-velha/src/br/unerj/poo/jogos/Jogo.java
+++ jogo-da-velha/src/br/unerj/poo/jogos/Jogo.java
@@ -1,9 +1,9 @@
 package br.unerj.poo.jogos;
 
 public class Jogo {
-	Jogador[] jogadores;
-	Tabuleiro tabuleiro;
-	Dado[] dado;
+	private Jogador[] jogadores;
+	private Tabuleiro tabuleiro;
+	private Dado[] dado;
 	private Jogador vez;
 	
 	public Jogo() {
@@ -20,14 +20,70 @@ public class Jogo {
 		// vazia
 		
 		boolean jogadaEhValida = 
-			tabuleiro.getCasa(x, y).isVazia(); 
+			tabuleiro.getPeca(x, y) == null; 
 		
 		if (jogadaEhValida) {
-			tabuleiro.setCasa(x, y, new Peca(vez));
-		}r
+			tabuleiro.setPeca(x, y, new Peca(vez));
+			passaVezParaProximoJogador();  // <-------
+		}
 		
 		return jogadaEhValida;
 	}
+    // |
+	// V
+	private void passaVezParaProximoJogador() {
+		vez = vez == jogadores[0]? jogadores[1] : jogadores[0];
+	}
+
+	public Jogador getJogadores(int i) {
+		return jogadores[i];
+	}
+
+	public Jogador getProximoJogador() {
+		return vez;
+	}
+
+	public boolean alguemGanhou() {
+		return 
+			fechouLinha(0) || 
+			fechouLinha(1) || 
+			fechouLinha(2) ||
+			fechouColuna(0) || 
+			fechouColuna(1) ||
+			fechouColuna(2) ||
+			fechouDiagonalPrincipal() ||
+			fechouDiagonalSecundaria();
+	}
 	
-	
+	private boolean fechouLinha(int i) {
+		boolean fechou = 
+			tabuleiro.getPeca(i, 0) != null &&
+			tabuleiro.getPeca(i, 0).equals(tabuleiro.getPeca(i, 1)) &&
+			tabuleiro.getPeca(i, 0).equals(tabuleiro.getPeca(i, 2));
+		return fechou;
+	}
+
+	private boolean fechouColuna(int i) {
+		boolean fechou = 
+			tabuleiro.getPeca(0, i) != null &&
+			tabuleiro.getPeca(0, i).equals(tabuleiro.getPeca(1, i)) &&
+			tabuleiro.getPeca(0, i).equals(tabuleiro.getPeca(2, i));
+		return fechou;
+	}
+
+	private boolean fechouDiagonalPrincipal() {
+		boolean fechou = 
+			tabuleiro.getPeca(0, 0) != null &&
+			tabuleiro.getPeca(0, 0) == tabuleiro.getPeca(1, 1) &&
+			tabuleiro.getPeca(0, 0) == tabuleiro.getPeca(2, 2);
+		return fechou;
+	}
+
+	private boolean fechouDiagonalSecundaria() {
+		boolean fechou = 
+			tabuleiro.getPeca(2, 0) != null  &&
+			tabuleiro.getPeca(2, 0) == tabuleiro.getPeca(1, 1) &&
+			tabuleiro.getPeca(2, 0) == tabuleiro.getPeca(0, 2);
+		return fechou;
+	}
 }
---
+++ jogo-da-velha/tests/br/unerj/poo/jogos/JogoTests.java
@@ -0,0 +1,58 @@
+package br.unerj.poo.jogos;
+
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JogoTests {
+	
+	Jogo jogo;                
+	Jogador j1, j2;
+
+	@Before
+	public void setUp() throws Exception {
+		jogo = new Jogo();
+		j1 = jogo.getJogadores(0);
+		j2 = jogo.getJogadores(1);
+	}
+	@After
+	public void tearDown() throws Exception {
+		jogo = null;
+		j1 = null;
+		j2 = null;
+	}
+	@Test
+	public void testVeSeJogaUmDeCadaVez() {
+		Assert.assertEquals(j1, jogo.getProximoJogador());
+		Assert.assertTrue(jogo.jogar(0, 0));
+		Assert.assertEquals(j2, jogo.getProximoJogador());
+		Assert.assertFalse(jogo.jogar(0, 0));
+		Assert.assertTrue(jogo.jogar(1, 1));
+		Assert.assertEquals(j1, jogo.getProximoJogador());
+	}
+	
+	@Test
+	public void testVeSeAlguemGanhou() {
+		jogo.jogar(0,0);
+		Assert.assertFalse(jogo.alguemGanhou());
+		
+		jogo.jogar(1,0);
+		Assert.assertFalse(jogo.alguemGanhou());
+		
+		jogo.jogar(0,1);
+		Assert.assertFalse(jogo.alguemGanhou());
+		
+		jogo.jogar(1,1);
+		Assert.assertFalse(jogo.alguemGanhou());
+		
+		jogo.jogar(0,2);
+		Assert.assertTrue(jogo.alguemGanhou());
+		
+//		Assert.assertEquals(j1, jogo.getVencedor())
+
+	}
+
+}
